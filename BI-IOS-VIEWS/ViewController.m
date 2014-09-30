02ad12878076b71297076bd099911b772d6bddd4
Dominik Vesely <vesely@dominoo.cz>
2014-09-30 10:23:15 +0200

    nase prvni view

--- BI-IOS-VIEWS/ViewController.m
+++ BI-IOS-VIEWS/ViewController.m
@@ -15,8 +15,27 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    CGRect firstFrame = CGRectMake(20, 20, 200, 200);
+    UIView* firstView = [[UIView alloc] initWithFrame:firstFrame];
+    firstView.backgroundColor = [UIColor blueColor];
+    
+    [self.view addSubview:firstView];
+
+    UIView* secondView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    secondView.backgroundColor = [UIColor redColor];
+    secondView.layer.cornerRadius = 8;
+    [firstView addSubview:secondView];
+
+    
+    
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    NSLog(@"ahoj bi-ios");
+    
 }
 
 - (void)didReceiveMemoryWarning {

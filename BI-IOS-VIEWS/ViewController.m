8e761a85b7608ae031a42a403d921ccf03c1ab1a
Dominik Vesely <vesely@dominoo.cz>
2014-09-30 07:43:32 +0200

    initial.

---
+++ BI-IOS-VIEWS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  BI-IOS-VIEWS
+//
+//  Created by Dominik Vesely on 30/09/14.
+//  Copyright (c) 2014 Ackee s.r.o. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

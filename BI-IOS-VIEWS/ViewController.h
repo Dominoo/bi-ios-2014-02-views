92a28d7323ab77b1cb10a8eb876666a655e8c209
Dominik Vesely <vesely@dominoo.cz>
2014-09-30 09:04:59 +0200

    tlacitka a akce

--- BI-IOS-VIEWS/ViewController.h
+++ BI-IOS-VIEWS/ViewController.h
@@ -10,6 +10,8 @@
 
 @interface ViewController : UIViewController
 
+- (IBAction)btnAction:(UIButton *)sender;
+
 
 @end
 
--- BI-IOS-VIEWS/ViewController.m
+++ BI-IOS-VIEWS/ViewController.m
@@ -9,12 +9,15 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISegmentedControl *segmentedControl;
+@property (weak, nonatomic) IBOutlet UIButton *myButton;
 
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
+
     [super viewDidLoad];
     
     CGRect firstFrame = CGRectMake(40, 40, 200, 200);
@@ -29,16 +32,34 @@
     secondView.backgroundColor = [UIColor redColor];
     [firstView addSubview:secondView];
     
+    UILabel* label = [[UILabel alloc] initWithFrame:CGRectMake(40, 210, 200, 40)];
+    label.textColor = [UIColor greenColor];
+    label.text = @"Chci spánek";
+    
+    [self.view addSubview:label];
     
     NSLog(@"ahoj bi-ios");
+    
+    [_segmentedControl addTarget:self action:@selector(segmentedAction:) forControlEvents:UIControlEventValueChanged];
     
     
     
 }
 
+
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (void) segmentedAction:(UISegmentedControl*) sender {
+    NSLog(@"selected segment is: %d",sender.selectedSegmentIndex);
+}
+
+- (IBAction)btnAction:(UIButton *)sender {
+    NSLog(@"Button pressed!");
+}
+
 
 @end
